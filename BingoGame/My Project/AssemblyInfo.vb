defebb583e1c18bd32a52f3c191b6a3c7a1b50de
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-02-20 08:21:33 -0700

    Add project files.

---
+++ BingoGame/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BingoGame")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("BingoGame")>
+<Assembly: AssemblyCopyright("Copyright © HP 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f4c07486-7a40-45c3-9dcb-40e96d460cc0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
