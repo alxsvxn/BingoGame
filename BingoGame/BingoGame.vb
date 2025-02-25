b2a0a0f3db76ad8d3a87ab287a711e1f0c90955d
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-02-25 08:53:04 -0700

    Added param, clear, and user input

--- BingoGame/BingoGame.vb
+++ BingoGame/BingoGame.vb
@@ -1,19 +1,32 @@
-﻿'Alexis Villagran
-
-Option Compare Binary
+﻿Option Compare Binary
 Option Strict On
 Option Explicit On
+'Alexis Villagran
+
+Imports System.Security.Authentication.ExtendedProtection
 
 Module BingoGame
-
     Sub Main()
-        For i = 1 To 10
+        Dim userInput As String
 
-            Drawball()
-            DisplayBoard()
-            Console.Read()
+        Do
             Console.Clear()
-        Next
+            DisplayBoard()
+            Console.WriteLine("Press d to Draw a ball")
+            userInput = Console.ReadLine()
+            Select Case userInput
+                Case "d"
+                    Drawball()
+                Case "c"
+                    BingoTracker(0, 0,, True)
+                Case Else
+
+            End Select
+
+        Loop Until userInput = "q"
+
+        Console.Clear()
+        Console.WriteLine("Have a Nice day!")
 
     End Sub
 
@@ -27,14 +40,27 @@ Module BingoGame
             currentBallInteger = RandomNumberBetween(0, 4)
         Loop Until temp(currentBallNumber, currentBallInteger) = False
 
-        BingoTracker(currentBallNumber, currentBallInteger)
+        BingoTracker(currentBallNumber, currentBallInteger, True)
         Console.WriteLine($"The current row is {currentBallNumber} and the column is {currentBallInteger}")
 
     End Sub
-    Function BingoTracker(ballNumber As Integer, ballLetter As Integer) As Boolean(,)
+    ''' <summary>
+    ''' contains a persistant array that tracks all possible balls 
+    ''' and whether they have been drawn during the current game
+    ''' </summary>
+    ''' <param name="ballNumber"></param>
+    ''' <param name="ballLetter"></param>
+    ''' <returns></returns>
+    Function BingoTracker(ballNumber As Integer, ballLetter As Integer, Optional update As Boolean = False, Optional clear As Boolean = False) As Boolean(,)
         Static _bingoTracker(14, 4) As Boolean
 
-        _bingoTracker(ballNumber, ballLetter) = True
+        If update Then
+            _bingoTracker(ballNumber, ballLetter) = True
+        End If
+
+        If clear Then
+            ReDim _bingoTracker(14, 4) ' clears the array. could also loop through array and set all elements
+        End If
 
         Return _bingoTracker
     End Function
@@ -42,7 +68,8 @@ Module BingoGame
     Sub DisplayBoard()
         Dim temp As String = " |"
         Dim heading() As String = {"B", "I", "N", "G", "O"}
-        Dim tracker(,) As Boolean = BingoTracker(0, 0)
+        Dim tracker(,) As Boolean = BingoTracker(0, 0) 'If BingoTracker is not there, tracker(,) won't work because it doesn't know any dimensions
+        'BingoTracker(0,0) is essentially ball 0,0 which just initializes the system
 
         For Each letter In heading
             Console.Write(letter.PadLeft(2).PadRight(4))
